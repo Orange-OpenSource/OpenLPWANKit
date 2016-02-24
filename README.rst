5232c95056fe8f90882bbd0c440c893e35b2823b
Cedric Pronzato <cedric.pronzato@orange.com>
2016-02-24 17:57:33 +0100

    update of opensource licensing informations

--- README.rst
+++ README.rst
@@ -56,7 +56,7 @@ See Samples ketch **>** Import library **>**
 
 License
 -------
+This software is distributed under the terms and conditions of the 'Apache-2.0' license which can be found in the file 'LICENSE.txt' in this package distribution or at 'http://www.apache.org/licenses/LICENSE-2.0'.
 
-
-Copyright (c) 2016 [Franck Roudet, Orange Labs]
+Copyright (c) 2016 Orange  - Franck Roudet
 
--- examples/01.Basics/leds.and.button/pushPinout.cpp
+++ examples/01.Basics/leds.and.button/pushPinout.cpp
@@ -1,9 +1,15 @@
 /*
- * Copyright (C) 2016 Franck Roudet
+ * Copyright (C) 2016 Orange - Franck Roudet
  *
  * This software is distributed under the terms and conditions of the 'Apache-2.0'
  * license which can be found in the file 'LICENSE.txt' in this package distribution
  * or at 'http://www.apache.org/licenses/LICENSE-2.0'.
+ */
+
+/* Open IoT Kit for LPWAN
+ *
+ * Version:     0.1.0
+ * Created:     2015-12-15 by Franck Roudet
  */
 
 #include "pushPinout.h"
